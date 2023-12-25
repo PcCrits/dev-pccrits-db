6c1aa0d69a017cb6e11bd934876652474c1c3545
julius legaspi <julius.legaspi@yachtly.com.sg>
2023-12-25 17:23:44 +0800

    implement populate info

--- app/services/v1/user/authenticate.js
+++ app/services/v1/user/authenticate.js
@@ -9,7 +9,7 @@ const authenticate = async (payload) => {
 	console.log('Invoke #authenticate()', payload, __filename)
 
 	try {
-		const user = await User.findOne({email: payload.email})
+		const user = await User.findOne({email: payload.email}).populate(['password', 'salt', '_id'])
 
 		if (!user) {
 			return {

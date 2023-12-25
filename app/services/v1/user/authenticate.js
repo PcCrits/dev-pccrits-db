61b31c040c79f9ad63596d03ade14b3e010cdc8e
julius legaspi <julius.legaspi@yachtly.com.sg>
2023-12-25 18:41:15 +0800

    enhance select query

--- app/services/v1/user/authenticate.js
+++ app/services/v1/user/authenticate.js
@@ -9,7 +9,8 @@ const authenticate = async (payload) => {
 	console.log('Invoke #authenticate()', payload, __filename)
 
 	try {
-		const user = await User.findOne({email: payload.email}).select('password')
+		const user = await User.findOne({email: payload.email})
+  .select('first_name', 'last_name', 'email')
 
 		if (!user) {
 			return {
